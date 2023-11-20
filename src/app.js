5c9af91937fef0f071a41720f6956615a26b6e47
DANNIMENNA89 <146239929+DANNIMENNA89@users.noreply.github.com>
2023-11-20 10:25:04 +0000

    Introducción HTML y JS

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let palos = [
+    "<div class='fs-1 text-danger border-0'>♦</div>",
+    "<div class='fs-1 text-danger border-0'>♥</div>",
+    "<div class='fs-1 border-0'>♠</div>",
+    "<div class='fs-1 border-0'>♣</div>"
+  ];
+  let numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let randomPalos = Math.floor(Math.random() * palos.length);
+  let randomNumero = Math.floor(Math.random() * numeros.length);
+  let elementPalos = document.getElementsByClassName("palo");
+  elementPalos[0].innerHTML = palos[randomPalos];
+  elementPalos[1].innerHTML = palos[randomPalos];
+  document.querySelector(".numero").innerHTML = numeros[randomNumero];
 };
